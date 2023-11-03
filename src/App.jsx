a6a5c7dfcc07d27ae8278d7900650ff0183e132e
karla.ortiz <karla.ortiz.martinez47@gmail.com>
2023-11-03 16:31:52 -0700

    data fetching working

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 //Dependency Imports
-import React from "react";
-import { useEffect, useState } from "react";
+import { useState, useEffect } from "react";
 import apiKey from "./config.js"
 import axios from "axios";
 import { Routes, Route } from 'react-router-dom'
@@ -8,26 +7,28 @@ import './index.css'
 
 //Component Imports
 import PhotoList from "./components/PhotoList.jsx";
-import Search from './components/Search';
-import Nav from './components/Nav';
+import Search from './components/Search.jsx';
+import Nav from './components/Nav.jsx';
 
 //App Component
 
 function App() {
-  const [query, setQuery] = useState('cars')
-  const fetchData = (query) => {
-    setQuery(query);
-  }
-  useEffect(() => {
-    axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`)
-  }, [query]);
+  const [data, setData] = useState([]);
+  const [query, setQuery] = useState('rainbows');
+  useEffect(()=>fetchData(query), [query]);
 
+  const fetchData = (query) =>{
+    axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`)
+    .then(response => setData(response.data.photos.photo))
+    .catch(error => console.log(error));
+  }
+  
   return (
     <div className='container'>
       <Search />
       <Nav />
       <Routes>
-        <Route path='/' />
+        <Route path='/' element={<PhotoList />}/>
         <Route path='/cats' element={<PhotoList />} />
         <Route path='/dogs' element={<PhotoList />} />
         <Route path='/computers' element={<PhotoList />} />
