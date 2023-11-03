4480e30afdc88b626ad6720c22dc198df9fd9d4b
karla.ortiz <karla.ortiz.martinez47@gmail.com>
2023-11-02 19:43:43 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# React Gallery App
+
+## Tech Degree Project 7: React Gallery App
---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import apiKey from "./config.js"
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello World</h1>
+      <p>{apiKey}</p>
+    </>
+  );
+    
+}
+
+export default App
