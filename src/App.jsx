289659b88e76c5514b7ee67ea5e6643341aa5b66
karla.ortiz <karla.ortiz.martinez47@gmail.com>
2023-11-03 13:31:17 -0700

    working on fetching data

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
-//import apiKey from "./config.js"
-import './index.css'
+//Dependency Imports
+import React from "react";
+import { useEffect, useState } from "react";
+import apiKey from "./config.js"
+import axios from "axios";
 import { Routes, Route } from 'react-router-dom'
+import './index.css'
 
 //Component Imports
 import PhotoList from "./components/PhotoList.jsx";
 import Search from './components/Search';
 import Nav from './components/Nav';
 
+//App Component
+
 function App() {
+  const [query, setQuery] = useState('cars')
+  const fetchData = (query) => {
+    setQuery(query);
+  }
+  useEffect(() => {
+    axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`)
+  }, [query]);
 
   return (
     <div className='container'>
@@ -16,13 +29,13 @@ function App() {
       <Routes>
         <Route path='/' />
         <Route path='/cats' element={<PhotoList />} />
-        <Route path='/dogs'element={<PhotoList />} />
+        <Route path='/dogs' element={<PhotoList />} />
         <Route path='/computers' element={<PhotoList />} />
         <Route path='/search/:query' element={<PhotoList />} />
       </Routes>
     </div>
   );
-    
+
 }
 
 export default App
