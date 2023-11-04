cff047daef4c4654f2dc6e3f4e50770250e88b3b
karla.ortiz <karla.ortiz.martinez47@gmail.com>
2023-11-04 11:40:20 -0700

    default page displays pictures

--- src/components/Photo.jsx
+++ src/components/Photo.jsx
@@ -1,11 +1,10 @@
 import PropTypes from "prop-types"
 
-const Photo = ({ photo }) => {
-    const server = photo.server;
-    const secret = photo.secret;
-    const id = photo.id;
-    const alt = photo.title;
-
+const Photo = ({ photoData }) => {
+    const server = photoData.server;
+    const secret = photoData.secret;
+    const id = photoData.id;
+    const alt = photoData.title;
     return (
         <li>
             <img src={`https://live.staticflickr.com/${server}/${id}_${secret}.jpg`} alt={`${alt}`} />
@@ -15,6 +14,7 @@ const Photo = ({ photo }) => {
 
 
 Photo.propTypes = {
-    photo: PropTypes.object
+    photoData: PropTypes.array.isRequired
 }
+
 export default Photo;
--- src/components/PhotoList.jsx
+++ src/components/PhotoList.jsx
@@ -2,14 +2,12 @@ import Photo from "./Photo.jsx";
 import PropTypes from "prop-types"
 
 const PhotoList = ({photos, title}) => {
-    console.log(photos)
+    
     return (
         <div className="photo-contianer">
             <h2>{title}</h2>
             <ul>
-                {photos.map(photo =>{
-                    <Photo key={photo.id} photo={photo} />
-                })}
+                {photos.map(photo => <Photo key={photo.id} photoData={photo} />)}
             </ul>
         </div>
     );
